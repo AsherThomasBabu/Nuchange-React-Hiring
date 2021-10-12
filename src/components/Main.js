bb894047df26f5dbdcb6e3c071ae5ba79544633b
AsherThomasBabu <ashertb2000@gmail.com>
2021-10-12 15:59:19 +0530

    Add placeholder for main component

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          {/* <!-- Replace with your content --> */}
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+          </div>
+          {/* <!-- /End replace --> */}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Main;
