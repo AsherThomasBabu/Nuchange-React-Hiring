6bfb35612798636faeed99af43bb5ee5969aa09c
AsherThomasBabu <ashertb2000@gmail.com>
2021-10-12 16:21:05 +0530

    hard code given data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
+import { data } from "autoprefixer";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const DATA = [
+    {
+      name: "Potato",
+      id: 1,
+      price: 30,
+      available: 1,
+      vendor: "Himachal Pvt Ltd",
+      category: "Vegtables",
+    },
+    {
+      name: "Banana",
+      id: 2,
+      price: 50,
+      available: 1,
+      category: "Fruits",
+      vendor: "Organic farms",
+    },
+    {
+      name: "Drumsticks",
+      id: 3,
+      price: 20,
+      available: 0,
+      category: "Vegetables",
+      vendor: "Mallikarjuna farms",
+    },
+    {
+      name: "Orange",
+      id: 4,
+      price: 25,
+      available: 1,
+      vendor: "Nagpur farms",
+      category: "Fruits",
+    },
+  ];
+
   return (
     <div>
       <Navbar />
       <Header />
-      <Main />
+      <Main data={DATA} />
     </div>
   );
 }
