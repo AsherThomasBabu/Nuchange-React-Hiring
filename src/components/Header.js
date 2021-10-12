169a80a2a8b44dc420914f593d0d1d22217d7708
AsherThomasBabu <ashertb2000@gmail.com>
2021-10-12 15:58:50 +0530

    Modify contents of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <header className="bg-white shadow">
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <header className="bg-purple-400 shadow">
+        <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl  text-center font-bold text-gray-900">
             Our Products
           </h1>
