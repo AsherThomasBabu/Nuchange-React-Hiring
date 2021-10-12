b47741b9c2a7702a51245911b5a03dc0973659f2
AsherThomasBabu <ashertb2000@gmail.com>
2021-10-12 16:56:50 +0530

    design template for product listing

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,24 @@ function Main() {
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* <!-- Replace with your content --> */}
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+            <div className="grid grid-cols-2">
+              <div className="border-2 flex justify-between p-5 m-5">
+                <div className="flex justify-items-start">
+                  <div className="">
+                    <img src="#" className="h-20 w-20"></img>
+                  </div>
+                  <div className="pl-4">
+                    <h4>Title</h4>
+                    <p>random text</p>
+                  </div>
+                </div>
+                <div className=" flex flex-col-reverse">
+                  <div className="flex flex-row-reverse items-center align-bottom">
+                    <button className="border-2 px-2 py-3">Add to Cart</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           {/* <!-- /End replace --> */}
         </div>
