6f93e0c25d3406fe5f8cb6384f4142159cd9a834
AsherThomasBabu <ashertb2000@gmail.com>
2021-10-12 16:04:19 +0530

    import components to main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
