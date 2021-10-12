beefc85a3eb29584f630a9ba0642dec20b94d58a
AsherThomasBabu <ashertb2000@gmail.com>
2021-10-12 15:30:08 +0530

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl  text-center font-bold text-gray-900">
+            Our Products
+          </h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
